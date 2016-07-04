58fd77781aa70521071cac45517fd9bf889a511a
Doowaw <quinncoral@gmail.com>
2016-07-04 19:19:31 -0400

    0.0.1-SNAPSHOT

--- src/main/java/co/gm4/GM4_UndeadPlayers/GM4_UndeadPlayers.java
+++ src/main/java/co/gm4/GM4_UndeadPlayers/GM4_UndeadPlayers.java
@@ -1,5 +1,13 @@
 package co.gm4.GM4_UndeadPlayers;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import co.gm4.GM4_UndeadPlayers.events.DeathEvent;
@@ -13,9 +21,71 @@ import co.gm4.GM4_UndeadPlayers.events.DeathEvent;
 
 public class GM4_UndeadPlayers extends JavaPlugin {
 	
+	public Class<?> iChatThing;
+	public Class<?> chatSerializer;
+	public Class<?> packetChatThing;
+	public Class<?> packetClass;
+	
+	private Method chatSerializer_a;
+	private Constructor<?> packetChatThing_;
+	
+	public String zombieName = "Undead Player";
+	public boolean canPickUpItems = true;
+	public boolean nameTagVisible = true;
+	public boolean canDespawn = false;
+	public List<String> accomplishedPlayers;
+	
 	@Override
 	public void onEnable()
 	{
+		String version = Bukkit.getServer().getClass().getName().replace(".", ",").split(",")[3] + ".";
+		String NMS_V = "net.minecraft.server." + version;
+		
+		String iChatString = NMS_V + "IChatBaseComponent";
+		String chatSerializerString = NMS_V + "IChatBaseComponent$ChatSerializer";
+		String packetChatString = NMS_V + "PacketPlayOutChat";
+		String packetClassString = NMS_V + "Packet";
+		
+		try {
+			this.iChatThing = Class.forName(iChatString);
+			this.chatSerializer = Class.forName(chatSerializerString);
+			this.packetChatThing = Class.forName(packetChatString);
+			this.packetClass = Class.forName(packetClassString);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return;
+		}
+		
+		this.saveDefaultConfig();
+		
+		this.zombieName = this.getConfig().getString("zombieName");
+		this.canPickUpItems = this.getConfig().getBoolean("canPickUpItems");
+		this.nameTagVisible = this.getConfig().getBoolean("nameTagVisible");
+		this.canDespawn = this.getConfig().getBoolean("canDespawn");
+		this.accomplishedPlayers = this.getConfig().getStringList("accomplishedPlayers");
+		
+		this.getConfig().set("zombieName", this.zombieName);
+		this.getConfig().set("canPickUpItems", this.canPickUpItems);
+		this.getConfig().set("nameTagVisible", this.nameTagVisible);
+		this.getConfig().set("canDespawn", this.canDespawn);
+		this.getConfig().set("accomplishedPlayers", this.accomplishedPlayers);
+		
+		this.saveConfig();
+		
+		try {
+			this.chatSerializer_a = this.chatSerializer.getMethod("a", String.class);
+			this.packetChatThing_ = this.packetChatThing.getConstructor(iChatThing, byte.class);
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return;
+		}
+		
 		new DeathEvent(this);
 	}
 	
@@ -25,4 +95,119 @@ public class GM4_UndeadPlayers extends JavaPlugin {
 		
 	}
 	
+	public Object getPlayerConnection(Player player)
+	{
+		Method getHandle;
+		try {
+			getHandle = player.getClass().getMethod("getHandle");
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return null;
+		}
+		
+	    Object nmsPlayer;
+		try {
+			nmsPlayer = getHandle.invoke(player);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return null;
+		}
+		
+	    Field conField;
+		try {
+			conField = nmsPlayer.getClass().getField("playerConnection");
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return null;
+		}
+		
+		Object con;
+		
+	    try {
+	    	con = conField.get(nmsPlayer);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return null;
+		}
+	    
+		return con;
+	}
+	
+	public Object makeTellraw(String json)
+	{
+		Object tellraw;
+		
+		try {
+			tellraw = chatSerializer_a.invoke(null, json);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return null;
+		}
+		
+		if(this.iChatThing.isInstance(tellraw))
+			return tellraw;
+		else
+			return null;
+	}
+	
+	public Object makePacketPlayOutChat(Object tellraw, byte arg1)
+	{
+		Object packetPlayOutChat;
+		
+		try {
+			packetPlayOutChat = packetChatThing_.newInstance(tellraw, arg1);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return null;
+		}
+		
+		if(packetChatThing.isInstance(packetPlayOutChat))
+			return packetPlayOutChat;
+		else
+			return null;
+	}
+	
+	public void sendPacket(Object connection, Object packet)
+	{
+		Method sendPacketMethod;
+		
+		try {
+			sendPacketMethod = connection.getClass().getMethod("sendPacket", this.packetClass);
+		} catch (NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return;
+		}
+		
+		try {
+			sendPacketMethod.invoke(connection, packet);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
+			
+			Bukkit.getPluginManager().disablePlugin(this);
+			
+			return;
+		}
+	}
 }
--- src/main/java/co/gm4/GM4_UndeadPlayers/events/DeathEvent.java
+++ src/main/java/co/gm4/GM4_UndeadPlayers/events/DeathEvent.java
@@ -1,5 +1,6 @@
 package co.gm4.GM4_UndeadPlayers.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
@@ -15,11 +16,11 @@ import co.gm4.GM4_UndeadPlayers.GM4_UndeadPlayers;
 
 public class DeathEvent implements Listener {
 
-	//private GM4_UndeadPlayers plugin;
+	private GM4_UndeadPlayers plugin;
 	
 	public DeathEvent(GM4_UndeadPlayers plugin)
 	{
-		//this.plugin = plugin;
+		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 	
@@ -30,6 +31,34 @@ public class DeathEvent implements Listener {
 		Location deathLocation = player.getLocation();
 		World deathWorld = player.getWorld();
 		
+		String uuid = player.getUniqueId().toString();
+		if(!(plugin.accomplishedPlayers.contains(uuid)))
+		{
+			plugin.accomplishedPlayers.add(uuid);
+			plugin.saveConfig();
+			
+			Player[] players_ = new Player[1];
+			Player[] players = Bukkit.getOnlinePlayers().toArray(players_);
+			
+			for(int i = 0; i < players.length; i++)
+			{
+				Object connection = plugin.getPlayerConnection(players[i]);
+				if(connection.equals(null))
+					return;
+				
+				Object tellraw = plugin.makeTellraw("{\"translate\":\"chat.type.achievement\",\"with\":[{\"text\":\"" + player.getDisplayName() + "\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"" + player.getDisplayName() + "\n" + uuid + "\"}},{\"text\":\"[Risen]\",\"color\":\"dark_aqua\",\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Risen\n\",\"color\":\"dark_aqua\"},{\"italic\":\"true\",\"text\":\"Achievement\n\",\"color\":\"white\"},{\"text\":\"Die and raise up an undead\nzombie in your body's place.\",\"color\":\"white\"}]}}}]}");
+				if(tellraw.equals(null))
+					return;
+				
+				Object packet = plugin.makePacketPlayOutChat(tellraw, (byte) 1);
+				if(packet.equals(null))
+					return;
+				
+				plugin.sendPacket(connection, packet);
+			}
+			
+		}
+		
 		Entity entity = deathWorld.spawnEntity(deathLocation, EntityType.ZOMBIE);
 		
 		boolean entityIsZombie = entity instanceof Zombie;
@@ -37,12 +66,12 @@ public class DeathEvent implements Listener {
 		
 		Zombie zombie = (Zombie) entity;
 		
-		zombie.setCustomName("Undead Player");
-		zombie.setCustomNameVisible(true);
+		zombie.setCustomName(plugin.zombieName);
+		zombie.setCustomNameVisible(plugin.nameTagVisible);
+		zombie.setRemoveWhenFarAway(plugin.canDespawn);
+		zombie.setCanPickupItems(plugin.canPickUpItems);
 		
 		zombie.setBaby(false);
 		zombie.setVillagerProfession(Villager.Profession.NORMAL);
-		
-		zombie.setRemoveWhenFarAway(false);
 	}
 }
